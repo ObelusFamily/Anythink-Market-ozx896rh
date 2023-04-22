8917ac1364d0c2cb0db1f619f25d33882805ac73
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-04-22 07:10:51 +0000

    tested in vs code and it worked. Having issues here though

---
+++ backend/helpers/index.js
@@ -0,0 +1,30 @@
+export const generateImage = async (prompt) => {
+  const size = "256x256";
+  const headers = {
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  };
+
+  const response = await axios.post(
+    "https://api.openai.com/v1/images/generations",
+    {
+      model: "image-alpha-001",
+      prompt: prompt,
+      size: size,
+      response_format: "url",
+    },
+    { headers: headers }
+  );
+
+  return response.data.data[0].url;
+};
+
+export const handleErrorResponse = (res, error) => {
+  console.error(error);
+  let message;
+  if (error instanceof Error) {
+    message = "An unexpected error occurred";
+  } else {
+    message = error.message || "Unknown error";
+  }
+  res.status(500).json({ message });
+};
